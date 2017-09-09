6d685b18c86323bb4af9655da59695c52d8ec866
Gustavo Apolinario <gustavoapolinario@MacBook-Pro-de-Gustavo.local>
2017-09-09 14:43:53 -0300

    Add static files and refatoring services paths

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 get_service() {
-	mkdir $1
-	git clone https://github.com/gugs89/$2 $1/$2
+	git clone https://github.com/gugs89/$1
 }
 
-get_service todo-backend microservices-node-example-todo-backend-mongodb
-get_service todo-frontend microservices-node-example-todo-frontend
+get_service microservices-node-example-todo-backend-mongodb
+get_service microservices-node-example-todo-frontend
+get_service microservices-static-files-example
 
